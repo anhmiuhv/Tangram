2d5516c60976b1be7e9ece20a8b0536f7f801e45
Setsuna <Setsuna@GundamOraiser>
2016-04-10 04:50:09 +0800

    square piece

--- Software engineering/src/model/Square.java
+++ Software engineering/src/model/Square.java
@@ -4,6 +4,11 @@ public class Square {
 	int sRow;
 	int sColumn;
 	
+	public Square(int sColumn, int sRow){
+		this.sColumn = sColumn;
+		this.sRow = sRow;		
+	}
+	
 	public int getColumn(){
 		return sColumn;
 	}
