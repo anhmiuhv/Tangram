bc97dda2db7eebbb4a2c4cfdd20d72a5da271623
linh <lthoang@wpi.edu>
2016-04-09 20:05:48 -0400

    update Kabasuji Builder

--- Software engineering/src/builder/model/KabasujiBuilder.java
+++ Software engineering/src/builder/model/KabasujiBuilder.java
@@ -8,4 +8,9 @@ public class KabasujiBuilder {
 	public KabasujiBuilder(ArrayList<LevelEditor> editor){
 		this.editor = editor;
 	}
+	
+	public int getNumberOfLevel(){
+//		return editor.size();
+		return 19;
+	}
 }
