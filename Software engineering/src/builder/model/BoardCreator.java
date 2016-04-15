ecf5571399f5fa33328874a11789bda1c3e66ca0
linh <lthoang@wpi.edu>
2016-04-15 18:31:40 -0400

    create Level Editor State

--- Software engineering/src/builder/model/BoardCreator.java
+++ Software engineering/src/builder/model/BoardCreator.java
@@ -1,10 +1,14 @@
 package builder.model;
-import model.*;
+import java.util.ArrayList;
+
+import model.Square;
+import model.Hint;
 public class BoardCreator {
 	Square[] squares = new Square[144];
 	Board board;
 	boolean[] selectedSquare = new boolean[144];
 	Hint hints;
+	int selected;
 	
 	public BoardCreator(){
 		int row = 0;
@@ -37,5 +41,70 @@ public class BoardCreator {
 		return squares;
 	}
 
+	public Board getBoard() {
+		return board;
+	}
+
+	public void setBoard(Board board) {
+		this.board = board;
+	}
+
+	public Hint getHints() {
+		return hints;
+	}
+
+	public void setHints(Hint hints) {
+		this.hints = hints;
+	}
+
+	public void setSquares(Square[] squares) {
+		this.squares = squares;
+	}
+
+	public int getNumSelect(){
+		return selected;
+	}
+	
+	/**
+	 * select square of i coordinate
+	 * @param i coordinate
+	 */
+	public void selectSquare(int i){
+		selectedSquare[i] = true;
+		selected++;
+	}
+	
+	/**
+	 * deselect square of i coordinate
+	 * @param i
+	 */
+	public void deselectSquare(int i){
+		selectedSquare[i] = false;
+		selected--;
+	}
+	
+	/**
+	 * create board from the selected squares
+	 */
+	public void createBoard(){
+		int row = 0;
+		int col = 0;
+		ArrayList<Square> s = new ArrayList<Square>();
+		for (int i = 0; i < 144; i++){
+			if (selectedSquare[i]){
+				Square n = new Square(col, row);
+				s.add(n);
+			}
+			col++;
+			if (col > 11){
+				col = 0;
+				row++;
+			}
+		}
+		
+		Square[] result = (Square[]) s.toArray();
+		board = new Board(result);
+		
+	}
 
 }
--- Software engineering/src/model/Hint.java
+++ Software engineering/src/model/Hint.java
@@ -1,6 +1,10 @@
 package model;
 
-public class Hint {
+public class Hint implements java.io.Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1954472508848657347L;
 	Square[] square;
 	public Hint(Square[] square){
 		this.square=square;
